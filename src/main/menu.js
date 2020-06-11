2dfb5fc9059ddd7a1f95001739fcfb7823bdd196
jemuk <wpanr77@gmail.com>
2020-06-11 18:25:32 +0900

    묵-회원가입폼(입력창)

--- src/main/menu.js
+++ src/main/menu.js
@@ -12,6 +12,23 @@ class menu extends Component {
     login: false,
     modalOpen: false,
     modalno: 0,
+    singno: 0,
+    singup: true,
+    modelid: "login-modal",
+  };
+
+  SingupClick = () => {
+    this.setState({
+      singup: false,
+      modelid: "singup-modal",
+    });
+  };
+
+  SinginClick = () => {
+    this.setState({
+      singup: true,
+      modelid: "login-modal",
+    });
   };
   LoginClick = () => {
     console.log(this.state.modalOpen);
@@ -62,42 +79,150 @@ class menu extends Component {
           </a>
         </div>
 
-        <Model isOpen={this.state.modalOpen} id="login-modal">
-          <div id="login-up">
-            <i class="fas fa-pencil-alt"></i>
-          </div>
-          <div id="login-tit">로그인</div>
-          <div id="login-box">
-            <div className="login-lable">아이디</div>
-            <input
-              type="text"
-              className="login-input"
-              placeholder="아이디"
-            ></input>
-            <div className="login-i"></div>
-          </div>
-          <div id="login-box2">
-            <div className="login-lable">비밀번호</div>
-            <input
-              type="password"
-              className="login-input"
-              placeholder="비밀번호"
-            ></input>
-            <div className="login-i"></div>
-          </div>
-          <a id="login-find" href="#">
-            아이디 / 비밀번호 분실
-          </a>
-          <div>
-            <button id="login-btn" onClick={this.LoginClick.bind(this)}>
-              로그인
-            </button>
-          </div>
-          <div>
-            <button id="login-btn2" onClick={this.LoginClick.bind(this)}>
-              취소
-            </button>
-          </div>
+        <Model isOpen={this.state.modalOpen} id={this.state.modelid}>
+          {this.state.singup && (
+            <div id="singin">
+              <div id="login-up" onClick={this.SingupClick.bind(this)}>
+                <i class="fas fa-pencil-alt"></i>
+              </div>
+              <div id="login-tit">로그인</div>
+              <div id="login-box">
+                <div className="login-lable">아이디</div>
+                <input
+                  type="text"
+                  className="login-input"
+                  placeholder="아이디"
+                ></input>
+                <div className="login-i"></div>
+              </div>
+              <div id="login-box2">
+                <div className="login-lable">비밀번호</div>
+                <input
+                  type="password"
+                  className="login-input"
+                  placeholder="비밀번호"
+                ></input>
+                <div className="login-i"></div>
+              </div>
+              <a id="login-find" href="#">
+                아이디 / 비밀번호 분실
+              </a>
+              <div>
+                <button id="login-btn" onClick={this.LoginClick.bind(this)}>
+                  로그인
+                </button>
+              </div>
+              <div>
+                <button id="login-btn2" onClick={this.LoginClick.bind(this)}>
+                  취소
+                </button>
+              </div>
+            </div>
+          )}
+          {!this.state.singup && (
+            <div id="singup">
+              <div id="singup-sid"></div>
+              <div id="singup-text">
+                <div id="singup-tit">회원가입</div>
+                <div className="singup-box">
+                  <div className="singup-lable">이름</div>
+                  <input
+                    type="password"
+                    className="singup-input"
+                    placeholder="이름"
+                  ></input>
+                  <div className="singup-i"></div>
+                </div>
+
+                <div className="singup-box">
+                  <div className="singup-lable">아이디</div>
+                  <input
+                    type="password"
+                    className="singup-input"
+                    placeholder="아이디"
+                  ></input>
+                  <div className="singup-i"></div>
+                </div>
+
+                <div className="singup-box">
+                  <div className="singup-lable">비밀번호</div>
+                  <input
+                    type="password"
+                    className="singup-input"
+                    placeholder="비밀번호"
+                  ></input>
+                  <div className="singup-i"></div>
+                </div>
+
+                <div className="singup-box">
+                  <div className="singup-lable">비밀번호 확인</div>
+                  <input
+                    type="password"
+                    className="singup-input"
+                    placeholder="비밀번호 확인"
+                  ></input>
+                  <div className="singup-i"></div>
+                </div>
+
+                <div className="singup-box">
+                  <div className="singup-lable">이메일</div>
+                  <input
+                    type="password"
+                    className="singup-input"
+                    placeholder="이메일"
+                  ></input>
+                  <div className="singup-i"></div>
+                </div>
+
+                <div className="singup-box">
+                  <div className="singup-lable">전화번호</div>
+                  <input
+                    type="password"
+                    className="singup-input"
+                    placeholder="전화번호"
+                  ></input>
+                  <div className="singup-i"></div>
+                </div>
+
+                <div className="singup-box">
+                  <div className="singup-lable">주소</div>
+                  <input
+                    type="password"
+                    className="singup-input"
+                    placeholder="주소"
+                  ></input>
+                  <div className="singup-i"></div>
+                </div>
+
+                <div className="singup-box">
+                  <div className="singup-lable">상세주소</div>
+                  <input
+                    type="password"
+                    className="singup-input"
+                    placeholder="상세주소"
+                  ></input>
+                  <div className="singup-i"></div>
+                </div>
+
+                <div>
+                  <button
+                    id="singup-btn"
+                    className="singup-btn"
+                    onClick={this.SinginClick.bind(this)}
+                  >
+                    회원가입
+                  </button>
+                  <button
+                    className="singup-btn"
+                    onClick={this.SinginClick.bind(this)}
+                  >
+                    취소
+                  </button>
+                </div>
+                <div></div>
+              </div>
+            </div>
+          )}
         </Model>
 
         {this.state.on && (
