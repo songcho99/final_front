338f5aa5dc069891faf8a87b78fc9e32083a33b3
jemuk <wpanr77@gmail.com>
2020-06-15 12:40:28 +0900

    마이페이지(정보수장+마이스터디)

--- src/MyPage/mypagemenu.js
+++ src/MyPage/mypagemenu.js
@@ -1,42 +1,46 @@
-import React,{Component} from'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./mypagemenu.css";
 
-class MyPageMenu extends Component{
-    render(){
-        return(
-            <div>
-                <Link to="/mypageupdate">
-                    <button>정보 수정</button>
-                </Link> &nbsp;&nbsp;
-                {/* 일반 회원만 */}
-                <Link to="/signup">
-                    <button>수강신청 현황</button>
-                </Link>  &nbsp;&nbsp;
-
-                {/* 매니저만 */}
-                <Link to="/signupmanage">
-                    <button>수강신청 관리</button>
-                </Link> &nbsp;&nbsp;
-                {/* 관리자만 */}
-                <Link to="/memberlist">
-                    <button>회원관리</button>
-                </Link> &nbsp;&nbsp;
-                <Link to="/graph">
-                    <button>통계</button>
-                </Link> &nbsp;&nbsp;
-                {/* 수강생만 */}
-                <Link to="/mystudymain">
-                    <button>마이 스터디</button>
-                </Link> &nbsp;&nbsp;
-                <Link to="/myclass">
-                <button type="button">내 학습공간</button>
-                </Link> &nbsp;&nbsp;
-                 {/*강사만 */}
-                 <Link to="/classmanage">
-                     <button>수업 관리</button>
-                 </Link>
-            </div>
-        )
-    }
+class MyPageMenu extends Component {
+  render() {
+    return (
+      <div id="mypageMenu">
+        <a
+          href="/mypageupdate"
+          className="mypagemenu-link"
+          id="mypagemenu-active"
+        >
+          정보 수정
+        </a>
+        {/* 일반 회원만 */}
+        <a href="/signup" className="mypagemenu-link">
+          수강신청 현황
+        </a>
+        {/* 매니저만 */}
+        <a href="/signupmanage" className="mypagemenu-link">
+          수강신청 관리
+        </a>
+        {/* 관리자만 */}
+        <a href="/memberlist" className="mypagemenu-link">
+          회원관리
+        </a>
+        <a href="/graph" className="mypagemenu-link">
+          통계
+        </a>
+        {/* 수강생만 */}
+        <a href="/mystudymain" className="mypagemenu-link">
+          마이 스터디
+        </a>
+        <a href="/myclass" className="mypagemenu-link">
+          내 학습공간
+        </a>
+        {/*강사만 */}
+        <a href="/classmanage" className="mypagemenu-link">
+          수업 관리
+        </a>
+      </div>
+    );
+  }
 }
 export default MyPageMenu;
