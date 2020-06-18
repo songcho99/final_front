423be480214d3f4e902a57958ef4d5d11de2cef8
hwcho96 <hwcho96@naver.com>
2020-06-18 12:26:34 +0900

    addstudy.js 수정

--- src/Study/addstudy.js
+++ src/Study/addstudy.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import DateFnsUtils from "@date-io/date-fns";
 import {
   MuiPickersUtilsProvider,
@@ -20,6 +20,8 @@ import {
 } from "@material-ui/core";
 import { PhotoCamera } from "@material-ui/icons";
 import "./addstudy.css";
+import Axios from "axios";
+import defaultimage from "../image/studytestimage.jpg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -68,25 +70,66 @@ function valuetext() {
   return marks.label;
 }
 
+function getFormatDate(date) {
+  var year = date.getFullYear(); // yyyy
+  var month = 1 + date.getMonth(); // M
+  month = month >= 10 ? month : "0" + month; // month 두자리로 저장
+  var day = date.getDate(); // d
+  day = day >= 10 ? day : "0" + day; // day 두자리로 저장
+  return year + "-" + month + "-" + day; // '-' 추가하여 yyyy-mm-dd 형태 생성
+}
+
 export default function AddStudy(props) {
+  const post = () => {
+    new window.daum.Postcode({
+      oncomplete: function (data) {
+        const zonecode = data.zonecode;
+        const roadAddr = data.address;
+        const str = "(" + zonecode + ")" + roadAddr;
+        setStudyAddress(str);
+      },
+    }).open();
+  };
+
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    script.src =
+      "https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+  });
+
   const [study_type, setStudyType] = React.useState("");
   const [study_subject, setStudySubject] = React.useState("");
   const [study_startdate, setStudyStartdate] = React.useState(
-    new Date().setDate(new Date().getDate() + 1)
+    getFormatDate(new Date())
   );
   const [study_enddate, setStudyEnddate] = React.useState(
-    new Date().setDate(new Date().getDate() + 1)
+    getFormatDate(new Date())
   );
-  const [study_gatherday, setStudyGatherday] = React.useState({
-    Monday: false,
-    Tuesday: false,
-    Wednesday: false,
-    Thursday: false,
-    Friday: false,
-    Saturday: false,
-    Sunday: false,
-  });
-  const [study_peoples, setStudyPeoples] = React.useState("");
+  const [study_gatherday, setStudyGatherday] = React.useState(
+    // [
+    //   { gather: "Monday", stat: false },
+    //   { gather: "Tuesday", stat: false },
+    //   { gather: "Wednesday", stat: false },
+    //   { gather: "Thursday", stat: false },
+    //   { gather: "Friday", stat: false },
+    //   { gather: "Saturday", stat: false },
+    //   { gather: "Sunday", stat: false },
+    // ]
+    {
+      Monday: false,
+      Tuesday: false,
+      Wednesday: false,
+      Thursday: false,
+      Friday: false,
+      Saturday: false,
+      Sunday: false,
+    }
+  );
+  const [study_peoples, setStudyPeoples] = React.useState(2);
   const [study_level, setStudyLevel] = React.useState(0);
   const [study_intr, setStudyIntr] = React.useState("");
   const [study_goal, setStudyGoal] = React.useState("");
@@ -94,7 +137,6 @@ export default function AddStudy(props) {
   const [study_address, setStudyAddress] = React.useState("");
   const [study_detailaddr, setStudyDetailaddr] = React.useState("");
   const count = [
-    1,
     2,
     3,
     4,
@@ -115,6 +157,10 @@ export default function AddStudy(props) {
     19,
     20,
   ];
+  const [study_mainimage, setStudyMainImage] = React.useState("");
+  const [file, setFile] = React.useState("");
+  const [previewURL, setPreviewURL] = React.useState("");
+  const [previewimg, setPreviewImg] = React.useState("");
 
   const countList = count.map((count, idx) => (
     <MenuItem value={count} key={idx}>
@@ -135,6 +181,7 @@ export default function AddStudy(props) {
       ...study_gatherday,
       [event.target.name]: event.target.checked,
     });
+
     console.log(study_gatherday);
   };
   const handleStartDateChange = (date) => {
@@ -146,7 +193,7 @@ export default function AddStudy(props) {
       alert("시작 날짜는 끝 날짜 이후를 선택 불가능합니다");
       return false;
     }
-    setStudyStartdate(date);
+    setStudyStartdate(getFormatDate(date));
   };
   const handleEndDateChange = (date) => {
     if (new Date() > date) {
@@ -157,7 +204,7 @@ export default function AddStudy(props) {
       alert("시작 날짜보다 이전 날짜는 선택 불가능합니다");
       return false;
     }
-    setStudyEnddate(date);
+    setStudyEnddate(getFormatDate(date));
   };
   const handlePeoplesChange = (event) => {
     setStudyPeoples(event.target.value);
@@ -187,11 +234,96 @@ export default function AddStudy(props) {
     setStudyDetailaddr(event.target.value);
     console.log(`detailaddr:${study_detailaddr}`);
   };
+  const handleImageChange = (event) => {
+    event.preventDefault();
+    const reader = new FileReader();
+    const file = event.target.files[0];
+    reader.onloadend = () => {
+      setFile(file);
+      setPreviewURL(reader.result);
+    };
+    reader.readAsDataURL(file);
+    setStudyMainImage(file);
+  };
 
-  const handleClick = () => {};
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const url = "http://localhost:8000/project/study/add";
+    const formData = new FormData();
+    study_level === 0 || study_level === "하"
+      ? setStudyLevel("하")
+      : study_level === 50 || study_level === "중"
+      ? setStudyLevel("중")
+      : study_level === 100 || study_level === "상"
+      ? setStudyLevel("상")
+      : setStudyLevel("");
+    console.log(study_level);
+
+    // if (study_gatherday.Monday)
+    //   setStudyGatherdayName(study_gatherdayname.concat("Monday"));
+    // if (study_gatherday.Tuesday)
+    //   setStudyGatherdayName(study_gatherdayname.concat("Tuesday"));
+    // if (study_gatherday.Wednesday)
+    //   setStudyGatherdayName(study_gatherdayname.concat("Wednesday"));
+    // if (study_gatherday.Thursday)
+    //   setStudyGatherdayName(study_gatherdayname.concat("Thursday"));
+    // if (study_gatherday.Friday)
+    //   setStudyGatherdayName(study_gatherdayname.concat("Friday"));
+    // if (study_gatherday.Saturday)
+    //   setStudyGatherdayName(study_gatherdayname.concat("Saturday"));
+    // if (study_gatherday.Sunday)
+    //   setStudyGatherdayName(study_gatherdayname.concat("Sunday"));
+    // console.log(study_gatherday);
+
+    console.log(study_gatherday);
+    formData.append("study_type", study_type);
+    formData.append("study_subject", study_subject);
+    formData.append("study_startdate", study_startdate);
+    formData.append("study_enddate", study_enddate);
+    formData.append("study_gatherday", study_gatherday);
+    formData.append("study_peoples", study_peoples);
+    formData.append("study_level", study_level);
+    formData.append("study_intr", study_intr);
+    formData.append("study_goal", study_goal);
+    formData.append("study_progress", study_progress);
+    formData.append("study_address", study_address);
+    formData.append("study_detailaddr", study_detailaddr);
+    formData.append("uploadfile", study_mainimage);
+    Axios({
+      method: "post",
+      url: url,
+      // data: {
+      //   study_type: study_type,
+      //   study_subject: study_subject,
+      //   study_startdate: study_startdate,
+      //   study_enddate: study_enddate,
+      //   study_gatherday: study_gatherday,
+      //   study_peoples: study_peoples,
+      //   study_level: study_level,
+      //   study_intr: study_intr,
+      //   study_goal: study_goal,
+      //   study_progress: study_progress,
+      //   study_address: study_address,
+      //   study_detailaddr: study_detailaddr,
+      // },
+      // data: { formData: formData },
+      data: formData,
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+      .then((res) => {
+        console.log(`데이터 추가:${res}`);
+      })
+      .catch((err) => {
+        console.log(`데이터 추가 오류:${err}`);
+      });
+  };
 
   const classes = useStyles();
   const sliderclasses = sliderStyles();
+
+  useEffect(() => {
+    setPreviewImg(<img alt="" src={previewURL} width="400"></img>);
+  }, [previewURL]);
 
   return (
     <div id="addstudyback2">
@@ -206,6 +338,7 @@ export default function AddStudy(props) {
         noValidate
         autoComplete="off"
         id="addstudyback"
+        onSubmit={onSubmit}
       >
         <div id="addstudynomargin">
           <div>
@@ -445,13 +578,14 @@ export default function AddStudy(props) {
               InputProps={{
                 readOnly: true,
               }}
+              value={study_address}
               onChange={handleAddressChange}
               required
             />
             <Button
               variant="contained"
               color="primary"
-              href="#contained-buttons"
+              onClick={post}
               style={{ marginTop: "20px" }}
             >
               검색
@@ -463,9 +597,6 @@ export default function AddStudy(props) {
             label="상세 주소"
             variant="outlined"
             style={{ zIndex: "0" }}
-            InputProps={{
-              readOnly: true,
-            }}
             onChange={handleDetailAddrChange}
             required
           />
@@ -477,8 +608,9 @@ export default function AddStudy(props) {
             accept="image/*"
             className={classes.input}
             id="contained-button-file"
-            multiple
             type="file"
+            name="study_mainimage"
+            onChange={handleImageChange}
           />
           <label htmlFor="contained-button-file">
             <Button
@@ -492,13 +624,15 @@ export default function AddStudy(props) {
           </label>
           <br />
           <br />
+          <div>{previewimg}</div>
+          <br />
+          <br />
           <div>
             <Button
               variant="contained"
               color="primary"
               type="submit"
               style={{ marginLeft: "640px" }}
-              onClick={handleClick}
             >
               개설하기
             </Button>
