d5b8e392e15fa8afab2ff2a8a265cc4a53b9e5e9
Chayongwoo <corsair457@naver.com>
2020-07-01 10:50:46 +0900

    알림 푸시

--- src/MyClass/ClassPage.js
+++ src/MyClass/ClassPage.js
@@ -7,6 +7,7 @@ class ClassPage extends Component {
     super(props);
 
     this.state = {
+      process_num: "",
       subject: "",
       teachername: "",
       manager: "",
@@ -34,6 +35,7 @@ class ClassPage extends Component {
       .then((res) => {
         console.log(res.data);
         this.setState({
+          process_num: res.data.process_num,
           subject: res.data.process_subject,
           teachername: res.data.process_teachername,
           manager: res.data.process_writer,
@@ -76,7 +78,7 @@ class ClassPage extends Component {
           <button>수업 자료</button>
         </Link>{" "}
         &nbsp;&nbsp;
-        <Link to="/mynote">
+        <Link to={"/AlertList/" + this.state.process_num}>
           <button>내 일일노트</button>
         </Link>{" "}
         &nbsp;&nbsp;
